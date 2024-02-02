80c6acba169d67d679a39bf1ff9c2290a53a4b49
rafat962 <rafatkamel96@gmail.com>
2024-02-02 22:39:15 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Barlow Semi Condensed','sans-serif'],
+      },
+      spacing: {
+        180: '32rem',
+      },
+    },
+  },
+  plugins: [],
+}
